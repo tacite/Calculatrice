30fa9cf79f9614608ab0c1a6b50b763536ba04d1
formation-de <formationcp1902@gmail.com>
2024-04-18 04:46:08 -0400

    Ajout de la fonctionnalité addition

---
+++ soustraction_division/__init__.py
@@ -0,0 +1,3 @@
+from add_div import add_op,div_op
+
+__all__ = {add_op,div_op}
---
+++ soustraction_division/add_div.py
@@ -0,0 +1,10 @@
+#
+# Disponible sous la branche addition
+#
+def add_op(a,b):
+    return a+b
+
+
+
+# def div_op(a,b):
+#     return a/b
