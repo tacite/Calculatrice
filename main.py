694b42ec12f4dfc55693ba86132e8619ce327a5d
David Tacite <david.tacite@gmail.com>
2024-04-18 12:10:34 +0200

    mise en package + gestion de la division

--- main.py
+++ main.py
@@ -8,42 +8,50 @@ def get_numbers():
     number_2 = float(input('Entrez le nombre 2: '))
     return number_1, number_2
 
-print(emoji.emojize('🫵  🫵  🫵    CALCULATRICE   🫵  🫵  🫵'))
-load_dotenv()
-historique = []
-if input('Rentrez votre mot de passe pour utiliser la calculatrice\n') != os.environ['MOT_DE_PASSE']:
-    quit()
-print("Menu:")
-print("1. Addition")
-print("2. Soustraction")
-print("3. Multiplication")
-print('4. Division')
+def start_calculator():
+    print(emoji.emojize('🫵  🫵  🫵    CALCULATRICE   🫵  🫵  🫵'))
+    load_dotenv()
+    historique = []
+    if input('Rentrez votre mot de passe pour utiliser la calculatrice\n') != os.environ['MOT_DE_PASSE']:
+        quit()
+    print("Menu:")
+    print("1. Addition")
+    print("2. Soustraction")
+    print("3. Multiplication")
+    print('4. Division')
 
-while True:
-    choice = input("Entrez votre choix (1 - 2 - 3 - 4 - <exit for exit> - <history for history) :")
-    match choice:
-        case '1':
-            a, b = get_numbers()
-            result = add_op(a, b)
-            historique.append(f'{a} + {b} = {result}')
-            print(result)
-        case '2':
-            a, b = get_numbers()
-            result = sous_op(a, b)
-            historique.append(f'{a} - {b} = {result}')
-            print(result)
-        case '3':
-            a, b = get_numbers()
-            result = mul_op(a, b)
-            historique.append(f'{a} * {b} = {result}')
-            print(result)
-        case '4':
-            a, b = get_numbers()
-            result = div_op(a, b)
-            historique.append(f'{a} / {b} = {result}')
-            print(result)
-        case 'history':
-            for calcul in historique:
-                print(calcul)
-        case 'exit':
-            break
+    while True:
+        choice = input("Entrez votre choix (1 - 2 - 3 - 4 - <exit for exit> - <history for history) :")
+        match choice:
+            case '1':
+                a, b = get_numbers()
+                result = add_op(a, b)
+                historique.append(f'{a} + {b} = {result}')
+                print(result)
+            case '2':
+                a, b = get_numbers()
+                result = sous_op(a, b)
+                historique.append(f'{a} - {b} = {result}')
+                print(result)
+            case '3':
+                a, b = get_numbers()
+                result = mul_op(a, b)
+                historique.append(f'{a} * {b} = {result}')
+                print(result)
+            case '4':
+                a, b = get_numbers()
+                if b == 0:
+                    print("Erreur division par 0")
+                else:
+                    result = div_op(a, b)
+                    historique.append(f'{a} / {b} = {result}')
+                    print(result)
+                        
+            case 'history':
+                for calcul in historique:
+                    print(calcul)
+            case 'exit':
+                break
+            
+if __name__ == '__main__':
+    start_calculator()
