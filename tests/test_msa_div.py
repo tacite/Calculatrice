611512e68f84692eefa8b84b81e52ad3ca8240b5
formation-de <formationcp1902@gmail.com>
2024-04-30 09:33:44 -0400

    msa - ajout add and div tests

---
+++ tests/test_msa_div.py
@@ -0,0 +1,13 @@
+from ..src.add_op import add_op
+from ..src.div_op import div_op
+import pytest
+
+def test_ok_div_op():
+    assert div_op(25,5) == 5
+    assert div_op(4.4,4) == 1.1
+
+
+def test_nok_add_op():
+    with pytest.raises(ZeroDivisionError):
+        assert add_op(4,[5])
+  
