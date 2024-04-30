2343bc094187e479cb9edc89972a9059183f5a94
David Tacite <david.tacite@gmail.com>
2024-04-30 15:51:44 +0200

    test toto

--- tests/test_msa_div.py
+++ tests/test_msa_div.py
@@ -4,10 +4,4 @@ import pytest
 
 def test_ok_div_op():
     assert div_op(25,5) == 5
-    assert div_op(4.4,4) == 1.1
-
-
-def test_nok_add_op():
-    with pytest.raises(ZeroDivisionError):
-        add_op(4,[5])
-  
+    assert div_op(4.4,4) == 1.1  
